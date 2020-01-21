48486b2b183f4c68833de74822fe5363ff388872
Sassine Aboujaoude <sassine.jaoude@hotmail.com>
2020-01-21 10:48:57 +0200

    Added sign in capabilities

---
+++ OneCap.Api/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OneCap.Bll.Dto.Request;
+using OneCap.Bll.Dto.Request.Account;
+using OneCap.Bll.Services;
+
+namespace OneCap.Api.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto registerUser, CancellationToken ct)
+        {
+            var user = new IdentityUser { UserName = registerUser.Email, Email = registerUser.Email };
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
